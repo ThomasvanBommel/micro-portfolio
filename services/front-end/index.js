22af4b721f0715a56843a65d7e1737a62af29f1d
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-09-25 15:13:13 -0300

    Added postgres database

--- services/front-end/index.js
+++ services/front-end/index.js
@@ -1,29 +1,43 @@
 const express = require("express");
+const { Client } = new require("pg");
 const http = require("http");
 const os = require("os");
+
 const app = express();
+const client = new Client();
 
-app.get("/", (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-                <title>Portfolio</title>
-            </head>
-            <body>
-                <div style="width:max-content;margin:0 auto;text-align:center;">
-                    <p>Welcome &#128151;</p>
-                    <code style="color:red;opacity:0.5;">${ os.hostname() }</code>
-                </div>
-            </body>
-        </html>
-    `);
+await client.connect({
+    host: "http://postgres-service",
+    database: "db",
+    password: "ABC123"
 });
 
-app.post("/register", (req, res) => {
-    http.post("http://register-service", resp => {
+app.get("/", async (req, res) => {
+    client.query("SELECT NOW()", (err, resp) => {
+        res.send(`
+            <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                    <title>Portfolio</title>
+                </head>
+                <body>
+                    <div style="width:max-content;margin:0 auto;text-align:center;">
+                        <p>Welcome &#128151;</p>
+                        <code style="color:red;opacity:0.5;">${ os.hostname() }</code>
+                        <br><br>
+                        Postgres Response:<br>
+                        <code>${ err ?? resp }</code>
+                    </div>
+                </body>
+            </html>
+        `);
+    });
+});
+
+app.post("/user", (req, res) => {
+    http.post("http://user-service", resp => {
         let data = '';
 
         resp.on("data", chunk => data += chunk);
